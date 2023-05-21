67db709d1083fdabccaca9d928f3f39e5f69d6a5
Vijay KC <vijaykc@Vijays-MacBook-Air.local>
2023-05-22 01:18:29 +0300

    integrated with mui pagination

---
+++ .history/client/src/components/AllJourneys_20230522003747.tsx
@@ -0,0 +1,172 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import TablePagination from "@mui/material/TablePagination";
+// import Pagination from "@mui/material/Pagination";
+import axios from "axios";
+import loader from "../assets/loading.gif";
+import JourneyRow from "./JourneyRow";
+import "../styles/Table.css";
+import "../styles/AllJourneys.css";
+import "react-toastify/dist/ReactToastify.css";
+
+const AllJourneys = () => {
+  interface journeyData {
+    _id: string;
+    departure_station_name: string;
+    departure_station_id: string;
+    departure: Date;
+    return_station_name: string;
+    return_station_id: string;
+    return: Date;
+    covered_distance: number;
+    duration: number;
+  }
+
+  const [data, setData] = useState<journeyData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(50);
+  const [totalRows, setTotalRows] = useState(2000000);
+
+  useEffect(() => {
+    const handleFetchData = async () => {
+      setLoading(true);
+      try {
+        const skip = (page - 1) * rowsPerPage;
+        const response = await axios.get(
+          `http://localhost:4000/api/journeys?limit=${rowsPerPage}&skip=${skip}`
+        );
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleFetchData();
+  }, [rowsPerPage]);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage + 1);
+  };
+
+  const handleChangeRowsPerPage = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(e.target.value, 0));
+    setPage(1);
+  };
+
+  // On input change.
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  //Items filtered based on
+  //a. Duration more than 10 seconds
+  //b. Distance more than 10 meters
+  //c. Station name in search input
+  //d. Station id in search input
+  const filteredData = data.filter(
+    (obj) =>
+      obj.duration > 10 &&
+      obj.covered_distance > 10 &&
+      (obj.departure_station_name
+        .toLowerCase()
+        .includes(search.toLowerCase()) ||
+        obj.departure_station_id === search)
+  );
+
+  if (loading) {
+    return (
+      <div className="flex py-[50px] justify-center items-center">
+        <img className="rounded-full" src={loader} alt="Loading icon" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrapper">
+      <ToastContainer autoClose={3000} />
+      <div className="flex flex-col md:flex-row md:justify-between md:items-bottom gap-4 mb-8">
+        <h2 className="text-3xl text-white font-bold">All Journeys</h2>
+      </div>
+      <table className="bg-white border border-white mx-auto w-full transition-all duration-300">
+        <caption className="relative text-left bg-white text-slate-500 border border-white">
+          <form>
+            <input
+              className="w-full h-full p-3 pr-12 focus:outline-4 focus:outline-slate-500"
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search by departure station or id.."
+              spellCheck="false"
+              autoComplete="off"
+              onChange={handleSearch}
+            ></input>
+            <button
+              disabled
+              className="material-icons absolute top-3 right-6 scale-125"
+            >
+              search
+            </button>
+          </form>
+        </caption>
+        <thead className=" border-collapse bg-orange-600 text-white">
+          <tr>
+            <th>Depart. Station (id)</th>
+            <th>Depart. Time</th>
+            <th>Return Station (id)</th>
+            <th>Return Time</th>
+            <th>Distance(km)</th>
+            <th>Duration(min, sec)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((obj) => {
+            return (
+              <JourneyRow
+                key={obj._id}
+                _id={obj._id}
+                dep_station={obj.departure_station_name}
+                dep_station_id={obj.departure_station_id}
+                dep_time={obj.departure}
+                return_station={obj.return_station_name}
+                return_station_id={obj.return_station_id}
+                return_time={obj.return}
+                distance={Math.round((obj.covered_distance / 1000) * 100) / 100}
+                duration={obj.duration}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="bg-white text-black w-full flex justify-center py-4">
+        <TablePagination
+          rowsPerPageOptions={[50, 100, 150, 200]}
+          component="div"
+          count={totalRows}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </div>
+
+      <button
+        className="inline-block btn-primary mt-6"
+        onClick={() => navigate(-1)}
+      >
+        &larr; Go Back
+      </button>
+    </div>
+  );
+};
+
+export default AllJourneys;
---
+++ .history/client/src/components/AllStations_20230521173612.tsx
@@ -0,0 +1,193 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import ReactPaginate from "react-paginate";
+import loader from "../assets/loading.gif";
+import StationRow from "./StationRow";
+import "../styles/Table.css";
+import "react-toastify/dist/ReactToastify.css";
+
+const AllStations = () => {
+  interface stationData {
+    _id: string;
+    station_id: string;
+    serial: number;
+    name: string;
+    address: string;
+    city: string;
+    operator: string;
+  }
+  const [data, setData] = useState<stationData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(25);
+  const [firstIndex, setFirstIndex] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(25);
+
+  const navigate = useNavigate();
+
+  // Page on load-- fetch all items
+  useEffect(() => {
+    handleFetchData(1000, 0);
+  }, []);
+
+  useEffect(() => {
+    if (itemsPerPage < 0) {
+      handleFetchData(itemsPerPage, 0).then(() => {
+        notify(data.length);
+      });
+    }
+  }, [itemsPerPage, data.length]);
+
+  // Providing two parameters for HTML change event,
+  const changeHandler = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    if (e.target instanceof HTMLInputElement) {
+      setSearch(e.target.value);
+    } else if (e.target instanceof HTMLSelectElement) {
+      setItemsPerPage(+e.target.value);
+    }
+  };
+
+  // const handleFetchData = async (items: number, offset: number) => {
+  const handleFetchData = async (page: number, perPage: number) => {
+    setLoading(true);
+    // const limit = items === undefined ? "" : items;
+    // const skip = offset === undefined ? "" : offset;
+
+    const res = await axios.get(
+      `http://localhost:4000/api/stations?page=${page}&perPage=${perPage}`
+    );
+    setData(res.data);
+    setLoading(false);
+  };
+
+  const notify = (items: number) => toast(`Showing ${items} items.`);
+
+  const filteredData = data.filter((obj) => {
+    return (
+      obj.name.toLowerCase().includes(search.toLowerCase()) ||
+      obj.station_id === search
+    );
+  });
+
+  // For Pagination //
+  const endIndex = firstIndex + itemsPerPage;
+  const currentItems = filteredData.slice(firstIndex, endIndex);
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  const handlePageClick = (e: any) => {
+    const newOffset = e.selected + 1 * itemsPerPage;
+    const maxOffset = data.length - itemsPerPage;
+    const clampedOffset = Math.min(newOffset, maxOffset);
+    setFirstIndex(clampedOffset);
+  };
+  ////
+
+  if (loading) {
+    return (
+      <div className="flex py-[50px] justify-center items-center">
+        <img src={loader} alt="Loading icon" />
+      </div>
+    );
+  }
+  return (
+    <div className="wrapper">
+      <ToastContainer autoClose={3000} />
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-8">
+        <h2 className="text-3xl text-white font-bold">All Stations</h2>
+        <form>
+          <select
+            defaultValue={"default"}
+            className="px-4 py-2 rounded-md"
+            id="itemsPerPage"
+            name="itemsPerPage"
+            onChange={changeHandler}
+          >
+            <option value="default" disabled>
+              Items per page
+            </option>
+            <option value="25">25 items</option>
+            <option value="50">50 items</option>
+            <option value="75">75 items</option>
+            <option value="100">100 items</option>
+          </select>
+        </form>
+      </div>
+      <table className="bg-white border border-white mx-auto w-full transition-all duration-300">
+        <caption className="relative text-left bg-white text-slate-500 border border-white">
+          <form>
+            <input
+              className="w-full h-full p-3 pr-12 focus:outline-4 focus:outline-slate-500"
+              type="text"
+              pattern="[a-z][0-9]"
+              id="search"
+              name="search"
+              placeholder="Search by station name or id.."
+              spellCheck={"false"}
+              onChange={changeHandler}
+            ></input>
+            <button
+              type="submit"
+              className="material-icons absolute top-3 right-6 scale-125"
+            >
+              search
+            </button>
+          </form>
+        </caption>
+        <thead className=" border-collapse bg-orange-600 text-white">
+          <tr>
+            <th>Station Name (id)</th>
+            <th>Address</th>
+            <th>City</th>
+            <th className="text-center">Capacity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((obj) => {
+            return (
+              <StationRow
+                _id={obj._id}
+                key={obj._id}
+                station_id={obj.station_id}
+                name={obj.name}
+                address={obj.address}
+                city={obj.city}
+                capacity={obj.capacity}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="bg-white text-black w-full">
+        <ReactPaginate
+          activeClassName={"item activePage "}
+          breakClassName={"item break-me "}
+          breakLabel={"..."}
+          containerClassName={"pagination"}
+          disabledClassName={"disabled-page"}
+          nextClassName={"item next "}
+          pageClassName={"item pagination-page "}
+          nextLabel="next >"
+          previousClassName={"item previous"}
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+        />
+      </div>
+      <button
+        className="inline-block btn-primary mt-6"
+        onClick={() => navigate(-1)}
+      >
+        &larr; Go Back
+      </button>
+    </div>
+  );
+};
+
+export default AllStations;
