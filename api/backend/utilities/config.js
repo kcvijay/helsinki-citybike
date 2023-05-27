fcca7844ec1e00a4730c0c0bc61d6f901a124159
Vijay KC <vijaykc@Vijays-MacBook-Air.local>
2023-05-27 20:11:55 +0300

    js files removed

--- api/backend/utilities/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URI = exports.PORT = void 0;
-require("dotenv").config();
-exports.PORT = process.env.PORT || 4000;
-exports.MONGO_URI = process.env.MONGO_URI;
